4dc9af9ba2de255ed4c5d8202961229a8f16e3db
onadikuda <37548585+onadikuda@users.noreply.github.com>
2019-06-11 16:09:15 +0530

    Delete RunSuite - Copy.bat

--- RunSuite - Copy.bat
+++
@@ -1,2 +0,0 @@
-pushd "E:/Performance/Automation workspace/AngularWithJava/"
-mvn clean test -Dfiles=WebContent/ReferenceFiles/Data/TestNG_Inputs/testngLocalhostIE.xml,WebContent/ReferenceFiles/Data/TestNG_Inputs/testngLocalhostChrome.xml
