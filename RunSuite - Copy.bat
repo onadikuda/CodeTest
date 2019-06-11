0caef5afda649fd281e3232146fa0cc19171f24d
onadikuda <37548585+onadikuda@users.noreply.github.com>
2019-06-11 12:34:05 +0200

    Add files via upload

---
+++ RunSuite - Copy.bat
@@ -0,0 +1,2 @@
+pushd "E:/Performance/Automation workspace/AngularWithJava/"
+mvn clean test -Dfiles=WebContent/ReferenceFiles/Data/TestNG_Inputs/testngLocalhostIE.xml,WebContent/ReferenceFiles/Data/TestNG_Inputs/testngLocalhostChrome.xml
---
+++ RunSuite.bat
@@ -0,0 +1,2 @@
+pushd "E:/Performance/Automation workspace/AngularWithJava/"
+mvn clean test -Dfiles=WebContent/ReferenceFiles/Data/TestNG_Inputs/testngLocalhostChrome.xml
